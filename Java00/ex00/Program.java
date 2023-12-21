29d6705ac2141a2d3e58ad492d28c9d0c9f5ac67
zakaria moumen <53499335+k34n4y138@users.noreply.github.com>
2023-12-21 14:09:27 +0000

    can't omit arguments as in C++

--- Java00/ex00/Program.java
+++ Java00/ex00/Program.java
@@ -3,10 +3,10 @@
  * Author: zmoumen <zmoumen@student.1337.ma>
  */
 
-package Java00.ex00;
+// package Java00.ex00;
 
 public class Program {
-	public static void main(String[]) {
+	public static void main(String[] args) {
 		int sum = 0;
 		for (int number = 1337;				/*subject specifies changing this number */
 			number > 0; number /= 10) {
