a088ffb50375f94aba88bf1d89fda9fe782b5d96
zakaria moumen <53499335+k34n4y138@users.noreply.github.com>
2023-12-21 14:35:40 +0000

    fix ex01

--- Java00/ex01/Program.java
+++ Java00/ex01/Program.java
@@ -3,34 +3,34 @@
  * Author: zmoumen <zmoumen@student.1337.ma>
  */
 
-package Java00.ex01;
-
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Program {
+    public static void main(String[] args) {
+        int input = 0;
+        int iters = 0;
+        Scanner scnr = new Scanner(System.in);
 
-    private int isPrime(int number) { /* Negative if not prime */
+        try {
+            input = scnr.nextInt();
+            iters = isPrime(input);
+            System.out.println((iters > 0) + " " + Math.abs(iters));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Illegal Argument");
+        } catch (Exception e) {
+            System.err.println("Error parsing input");
+        }
+    }
+
+    private static int isPrime(int number) throws Exception { /* Negative if not prime */
         if (number <= 1) {
-            raise "Illegal Argument";
+            throw new IllegalArgumentException("Illegal");
         }
         int iters = 1;
         for (int i = 2; i * i <= number; i++, iters++)
             if (number % i== 0)
                 return (-1 * iters);
         return (iters);
-    }
-    private int abs(int number) {
-        return ((number < 0)? -1 * number: number);
-    }
-    public static void main(String[]) {
-        try {
-            int input = Scanner(System.in).nextInt();
-            int iters = isPrime(input);
-        }
-        catch (string e) {
-            system.out.println(e);
-            return ;
-        }
-        system.out.println((iters < 0)? "false ": "true " + abs(iters));
     }
 }
